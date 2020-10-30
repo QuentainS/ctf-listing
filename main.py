11506be6d230156369ec4073be4a976e4a1fc899
Quentain Schietecatte <quentain.s@gmail.com>
2020-10-30 12:42:03 +0100

    [add] colors, final links

--- main.py
+++ main.py
@@ -2,19 +2,51 @@
 from ics import Calendar
 import requests
 import arrow
+from bcolors import bcolors
 
 
 def get_calendar():
+
     url = "https://calendar.google.com/calendar/ical/ctftime%40gmail.com/public/basic.ics"
     return Calendar(requests.get(url).text)
 
 
-def summarize(event):
+def get_link(ctftime_link):
+
+    # Fetch the webpage (must modify the User Agent)
+    html = requests.get(ctftime_link, headers={'User-Agent': 'Mozilla/5.0'})
+    html = html.text
+
+    # Sometimes, there are some mistakes in the links
+    if "Not found. Back to the" in html:
+        return bcolors.fail("404 Error")
+
+    # Get only the interesting part
+    start = html.find("Official URL") + 23
+    html = html[start:]
+    end = html.find("\" rel=\"nofollow\">")
+    html = html[:end]
+
+    return html
+
+
+def parse(event):
     res = {}
-    res['name'] = event.name
+    res['name'] = bcolors.colored(event.name, bcolors.BOLD)
     res['start'] = event.begin.to('local')
     res['end'] = event.end.to('local')
-    res['description'] = event.description
+
+    desc = event.description.split('\n')
+
+    if "Jeopardy" in desc[0]:
+        res['type'] = bcolors.ok("Jeopardy")  # Love Jeopardy
+    elif "Attack-Defense" in desc[0]:
+        res['type'] = bcolors.fail("Attack-Defense")  # Hate A-D
+    else:
+        res['type'] = bcolors.warning(desc[0])  # Oh, something new !
+
+    res['url'] = get_link(desc[1][5:])
+
     return res
 
 
@@ -26,7 +58,7 @@ def get_upcoming(calendar, day):
     # Get the event in [now ; now+day]
     for event in calendar.events:
         if now.shift(days=+day) > event.begin.to('local') > now:
-            upcoming.append(summarize(event))
+            upcoming.append(parse(event))
 
     # Sort the events
     upcoming = sorted(upcoming, key=lambda k: k['start'])
@@ -34,8 +66,20 @@ def get_upcoming(calendar, day):
     return upcoming
 
 
-calendar = get_calendar()
-events = get_upcoming(calendar, 15)
-
-for event in events:
+def show(event):
     print(event['start'].humanize())
+    print(event['name'])
+    print(event['start'])
+    print(event['end'])
+    print(event['type'])
+    print(event['url'])
+    print()
+
+
+if __name__ == "__main__":
+
+    calendar = get_calendar()
+    events = get_upcoming(calendar, 15)
+
+    for event in events:
+        show(event)
